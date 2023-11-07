cf43528e5f180818a67e4ddfe54d5f4022f3db83
Jobenas <jobenas@energyatech.com>
2023-11-07 01:43:42 +0000

    semana 13 clase 1 parte 2

---
+++ echo_server_multithreaded.py
@@ -0,0 +1,41 @@
+import socket
+from threading import Thread
+
+SOCK_BUFFER = 1024
+
+
+def client_handler(conn, client_address):
+    print(f"Conexion desde {client_address[0]}:{client_address[1]}")
+
+    try:
+        while True:
+            data = conn.recv(SOCK_BUFFER)
+
+            if data:
+                print(f"Recibido {data}")
+                conn.sendall(data)
+            else:
+                print(f"No hay mas datos")
+                break
+    except ConnectionResetError:
+        print("el cliente ha cerrado abruptamente la conexion")
+    finally:
+        print("El cliente ha cerrado la conexion")
+        conn.close()
+
+
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 5000)
+    print(f"Iniciando servidor en IP {server_address[0]} y puerto {server_address[1]}")
+
+    sock.bind(server_address)
+
+    sock.listen(1)
+
+    while True:
+        print("Esperando conexiones...")
+        connection, c_address = sock.accept()
+        
+        t = Thread(target=client_handler, args=(connection, c_address))
+        t.start()
